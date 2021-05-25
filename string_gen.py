2458f54f9ac3813c83becc58dd42a8c0ce9f2147
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2021-05-25 13:48:15 +0530

    Update string_gen.py

--- string_gen.py
+++ string_gen.py
@@ -2,18 +2,7 @@ import pyrogram
 from pyrogram import Client
 
 friday_ = """
-╔═══╗───╔═╗─╔═╗
-║ ══╬═╦═╬═╬═╝ ╠═══╦═╦═╗
-║ ╔═╣ ╔═╣ ║╔╗ ║╔╗ ╠══ ║
-╚═╝ ╚═╝ ╚═╩═══╩═╩═╩═══╝
-
-Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
-
-This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
-and is released under the "GNU v3.0 License Agreement".
-Please see < https://github.com/DevsExpo/blob/master/LICENSE >
-
-All rights reserved.
+satya
 """
 
 print(friday_)
